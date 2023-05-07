f8be980a70cf1dad96566483819913b07ffdee80
tck199732 <ctb8135@wmich.edu>
2023-05-07 04:33:19 -0400

    ✨add findbin, same behaviour as in ROOT

--- pycpa/utilities/histo.py
+++ pycpa/utilities/histo.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 class histogram_handler:
     @staticmethod
     def projection(df, cut=None, range=None, bins=None, axis='x', normalize=True):
@@ -34,50 +35,68 @@ class histogram_handler:
             df.columns = ['x', 'y', 'z', 'z_err', 'z_ferr']
         else:
             raise ValueError('Dataframe must have at most 5 columns')
-        
+
         cut_axis = 'y' if axis == 'x' else 'x'
         if cut is not None:
-            bw = df[cut_axis][1] - df[cut_axis][0]
-            cut = (cut[0] - bw/2, cut[1] + bw/2)
-            df.query(f'{cut_axis} > {cut[0]} & {cut_axis} < {cut[1]}', inplace=True)
+            df = histogram_handler.query(df, cut, cut_axis)
 
         range = (df[axis].min(), df[axis].max()) if range is None else range
         bins = df.shape[0] if bins is None else bins
 
         hist, edges = np.histogram(
-            df[axis], 
+            df[axis],
             weights=df['z'],
-            range=range, 
+            range=range,
             bins=bins
         )
 
         histerr, _ = np.histogram(
             df[axis],
-            weights=df['z_err']**2, 
+            weights=df['z_err']**2,
             range=range,
             bins=bins
         )
         histerr = np.sqrt(histerr)
         norm = (np.diff(range) / bins)[0] if normalize else 1.
-        
+
         return pd.DataFrame({
-            'x' : (edges[1:] + edges[:-1])/2,
-            'y' : hist / norm,
-            'y_err' : histerr / norm,
-            'y_ferr' : np.divide(histerr, hist, out=np.zeros_like(hist), where=hist!=0)
+            'x': (edges[1:] + edges[:-1])/2,
+            'y': hist / norm,
+            'y_err': histerr / norm,
+            'y_ferr': np.divide(histerr, hist, out=np.zeros_like(hist), where=hist != 0)
         })
 
     @staticmethod
-    def query(df, xrange, yrange=None):
+    def findbin(df, x, axis='x'):
+        """ Find the bin for a given array in the dataframe-histogram in the same way as FindBin in ROOT.
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Dataframe containing the histogram, with the bin centers in the specified axis.
+        x : array
+            Array of values to be binned.
+        axis : str, optional    
+            Axis along which the projection is performed. Must be 'x' or 'y'.
+        Returns
+        -------
+        array
+            Array of bin indices.
+        """
+        bincenters = np.unique(df[axis])
+        binwidth = bincenters[1] - bincenters[0]
+        bins = np.array([bincenters[0] - binwidth / 2] +
+                        list(bincenters + binwidth / 2))
+        return np.digitize(x=x, bins=bins, right=False)
+
+    @staticmethod
+    def query(df, range, axis='x'):
         df = df.copy()
-        xbw = np.diff(xrange)[0] / df.shape[0]
-        xrange = (xrange[0] - xbw/2, xrange[1] + xbw/2)
-        df.query(f'x > {xrange[0]} & x < {xrange[1]}', inplace=True)
-        if yrange is not None:
-            ybw = np.diff(yrange)[0] / df.shape[1]
-            yrange = (yrange[0] - ybw/2, yrange[1] + ybw/2)
-            df.query(f'y > {yrange[0]} & y < {yrange[1]}', inplace=True)
-        return df
+        # find the bin (start from 1), subtract 1 to get the index of array
+        bin1, bin2 = histogram_handler.findbin(df, range) - 1
+        x = np.unique(df[axis])
+        range = (x[bin1], x[bin2])
+        print(range)
+        return df.query(f'{axis} >= {range[0]} & {axis} <= {range[1]}')
 
     @staticmethod
     def rebin1D(df, range, bins=None, normalize=True):
@@ -93,7 +112,7 @@ class histogram_handler:
             df['y_err'] = 0
         else:
             raise ValueError('Dataframe must have at least 2 columns')
-        
+
         bins = df.shape[0] if bins is None else bins
         hist, edges = np.histogram(
             df['x'],
@@ -112,10 +131,10 @@ class histogram_handler:
         norm = (np.diff(range) / bins)[0] if normalize else 1.
 
         return pd.DataFrame({
-            'x' : (edges[1:] + edges[:-1])/2,
-            'y' : hist / norm,
-            'y_err' : histerr / norm,
-            'y_ferr' : np.divide(histerr, hist, out=np.zeros_like(hist), where=hist!=0)
+            'x': (edges[1:] + edges[:-1])/2,
+            'y': hist / norm,
+            'y_err': histerr / norm,
+            'y_ferr': np.divide(histerr, hist, out=np.zeros_like(hist), where=hist != 0)
         })
 
     @staticmethod
@@ -138,10 +157,10 @@ class histogram_handler:
             raise ValueError('Dataframes must have at least 3 columns')
 
         return pd.DataFrame({
-            'x' : a['x'],
-            axis : a[axis] * b[axis],
-            f'{axis}_err' : np.sqrt((a[f'{axis}_err'] * b[axis])**2 + (a[axis] * b[f'{axis}_err'])**2),
-            f'{axis}_ferr' : np.sqrt((a[f'{axis}_ferr'] * b[axis])**2 + (a[axis] * b[f'{axis}_ferr'])**2)
+            'x': a['x'],
+            axis: a[axis] * b[axis],
+            f'{axis}_err': np.sqrt((a[f'{axis}_err'] * b[axis])**2 + (a[axis] * b[f'{axis}_err'])**2),
+            f'{axis}_ferr': np.sqrt((a[f'{axis}_ferr'] * b[axis])**2 + (a[axis] * b[f'{axis}_ferr'])**2)
         })
 
     @staticmethod
@@ -162,19 +181,22 @@ class histogram_handler:
         """
         if not set(['x', 'y', 'y_err']).issubset(set(a.columns)) and set(['x', 'y', 'y_err']).issubset(set(b.columns)):
             raise ValueError('Dataframes must have at least 3 columns')
-        
-        if not 'y_ferr' in a.columns:
-            a['y_ferr'] = np.divide(a['y_err'], a['y'], out=np.zeros_like(a['y']), where=a['y']!=0)
-        if not 'y_ferr' in b.columns:
-            b['y_ferr'] = np.divide(b['y_err'], b['y'], out=np.zeros_like(b['y']), where=b['y']!=0)
 
-        y = np.divide(a[axis], b[axis], out=np.zeros_like(a[axis]), where=b[axis]!=0)
+        if not 'y_ferr' in a.columns:
+            a['y_ferr'] = np.divide(
+                a['y_err'], a['y'], out=np.zeros_like(a['y']), where=a['y'] != 0)
+        if not 'y_ferr' in b.columns:
+            b['y_ferr'] = np.divide(
+                b['y_err'], b['y'], out=np.zeros_like(b['y']), where=b['y'] != 0)
+
+        y = np.divide(a[axis], b[axis], out=np.zeros_like(
+            a[axis]), where=b[axis] != 0)
         y_err = y * np.sqrt(a[f'{axis}_ferr'] ** 2. + b[f'{axis}_ferr'] ** 2.)
         return pd.DataFrame({
-            'x' : a['x'],
-            axis : y,
-            f'{axis}_err' : y_err,
-            f'{axis}_ferr' : np.divide(y_err, y, out=np.zeros_like(y), where=y!=0)
+            'x': a['x'],
+            axis: y,
+            f'{axis}_err': y_err,
+            f'{axis}_ferr': np.divide(y_err, y, out=np.zeros_like(y), where=y != 0)
         })
 
     @staticmethod
@@ -198,20 +220,20 @@ class histogram_handler:
 
         if not set(['x', 'y', 'y_err']).issubset(set(a.columns)) and set(['x', 'y', 'y_err']).issubset(set(b.columns)):
             raise ValueError('Dataframes must have at least 3 columns')
-        
+
         y = a[axis] + b[axis] * sign
         y_err = np.sqrt(a[f'{axis}_err']**2 + b[f'{axis}_err']**2)
         return pd.DataFrame({
-            'x' : a['x'],
-            axis : y,
-            f'{axis}_err' : y_err,
-            f'{axis}_ferr' : np.divide(y_err, y, out=np.zeros_like(y), where=y!=0)
+            'x': a['x'],
+            axis: y,
+            f'{axis}_err': y_err,
+            f'{axis}_ferr': np.divide(y_err, y, out=np.zeros_like(y), where=y != 0)
         })
-    
+
     @staticmethod
     def add(a, b, axis='y'):
         return histogram_handler.sum(a, b, axis, sign=1.)
-    
+
     @staticmethod
     def sub(a, b, axis='y'):
         return histogram_handler.sum(a, b, axis, sign=-1.)
