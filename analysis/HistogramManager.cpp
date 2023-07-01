9b350dff0d3cc2f4001201c415a5c0d4debb0e70
tck199732 <ctb8135@wmich.edu>
2023-07-01 18:22:47 -0400

    :art: use singleton ame, add theta vs E hist

--- analysis/HistogramManager.cpp
+++ analysis/HistogramManager.cpp
@@ -3,78 +3,82 @@
 HistogramManager::HistogramManager(const std::string &str)
 {
     mStr = str;
-    mParticlesNames = this->PARTICLES;
+    mParticles = this->ACCEPTED_PARTICLES;
     this->_Initialize();
 }
 
 HistogramManager::~HistogramManager()
 {
-    for (auto &[str, hist] : h2_pta_rapidity_lab)
+    for (auto &[key, hist] : this->h2_pta_rapidity_lab)
     {
-        if (hist)
-        {
-            delete hist;
-        }
+        delete hist;
+    }
+
+    for (auto &[key, hist] : this->h2_theta_kinery_lab)
+    {
+        delete hist;
     }
 }
 
 void HistogramManager::Fill(const Particle &particle, const double &weight)
 {
-    std::string name = "";
-    if (particle.Z == 1 && particle.N == 0)
-    {
-        name = "p";
-    }
-    else if (particle.Z == 1 && particle.N == 1)
-    {
-        name = "d";
-    }
-    else if (particle.Z == 1 && particle.N == 2)
-    {
-        name = "t";
-    }
-    else if (particle.Z == 2 && particle.N == 1)
-    {
-        name = "3He";
-    }
-    else if (particle.Z == 2 && particle.N == 2)
-    {
-        name = "4He";
-    }
-    else
+    int A = particle.N + particle.Z;
+    std::string ame_name = AME::get_instance()->GetSymbol(particle.Z, A).value_or("");
+    if (this->h2_pta_rapidity_lab.count(ame_name) == 0)
     {
         return;
     }
-    if (this->h2_pta_rapidity_lab.count(name) == 0)
-    {
-        return;
-    }
-    this->h2_pta_rapidity_lab[name]->Fill(particle.rapidity_lab_normed, particle.pmag_trans / (particle.N + particle.Z), weight);
+    this->h2_pta_rapidity_lab[ame_name]->Fill(particle.rapidity_lab_normed, particle.pmag_trans / A, weight);
+    this->h2_theta_kinery_lab[ame_name]->Fill(particle.kinergy_lab / A, particle.theta_lab, weight);
     return;
 }
 
 void HistogramManager::Write()
 {
-    for (auto &p : this->mParticlesNames)
+    for (auto &p : this->mParticles)
     {
         this->h2_pta_rapidity_lab[p]->Write();
+        this->h2_theta_kinery_lab[p]->Write();
     }
 }
 
 void HistogramManager::Normalize(const double &norm)
 {
-    for (auto &p : this->mParticlesNames)
+    for (auto &p : this->mParticles)
     {
         this->h2_pta_rapidity_lab[p]->Scale(1. / norm);
+        this->h2_theta_kinery_lab[p]->Scale(1. / norm);
     }
 }
 
 void HistogramManager::_Initialize()
 {
-    for (auto &p : this->mParticlesNames)
+    auto ame = AME::get_instance();
+    for (auto &p : this->mParticles)
     {
-        this->h2_pta_rapidity_lab[p] = new TH2D(("h2_pta_rapidity_lab_" + mStr + "_" + p).c_str(), "", 150, 0, 1.5, 800, 0, 800);
-        this->h2_pta_rapidity_lab[p]->Sumw2();
-        this->h2_pta_rapidity_lab[p]->SetDirectory(0);
+        if (!ame->IsPhysical(p))
+        {
+            continue;
+        }
+        std::string ame_name = AME::get_instance()->GetSymbol(p).value_or("unknown");
+        this->h2_pta_rapidity_lab[ame_name] = new TH2D(("h2_pta_rapidity_lab_" + mStr + "_" + p).c_str(), "", 150, 0, 1.5, 800, 0, 800);
+
+        this->h2_theta_kinery_lab[ame_name] = new TH2D(("h2_theta_kinery_lab_" + mStr + "_" + p).c_str(), "", 250, 0, 250, 600, 20, 80);
+
+        this->h2_pta_rapidity_lab[ame_name]->Sumw2();
+        this->h2_theta_kinery_lab[ame_name]->Sumw2();
+        this->h2_pta_rapidity_lab[ame_name]->SetDirectory(0);
+        this->h2_theta_kinery_lab[ame_name]->SetDirectory(0);
+    }
+}
+
+void HistogramManager::SetAcceptedParticles(const std::vector<std::string> &particles)
+{
+    for (auto &p : particles)
+    {
+        if (std::find(this->ACCEPTED_PARTICLES.begin(), this->ACCEPTED_PARTICLES.end(), p) != this->ACCEPTED_PARTICLES.end())
+        {
+            this->mParticles.push_back(p);
+        }
     }
 }
--- analysis/HistogramManager.hh
+++ analysis/HistogramManager.hh
@@ -8,6 +8,7 @@
 
 #include "TH2D.h"
 
+#include "AME.hh"
 #include "Particle.hh"
 
 class HistogramManager
@@ -16,17 +17,19 @@ public:
     HistogramManager(const std::string &str);
     ~HistogramManager();
     void _Initialize();
+    void SetAcceptedParticles(const std::vector<std::string> &particles);
     void Fill(const Particle &particle, const double &weight = 1.);
     void Normalize(const double &norm);
     void Write();
 
 private:
     std::string mStr;
-    std::vector<std::string> mParticlesNames;
+    std::vector<std::string> mParticles;
     std::map<std::string, TH2D *> h2_pta_rapidity_lab;
+    std::map<std::string, TH2D *> h2_theta_kinery_lab;
 
 protected:
-    std::vector<std::string> PARTICLES = {
+    std::vector<std::string> ACCEPTED_PARTICLES = {
         "p",
         "d",
         "t",
